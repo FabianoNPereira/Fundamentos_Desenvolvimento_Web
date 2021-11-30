442fef30cc9511e3c3f59bc4c4456a99e68c38a6
FabianoPereira <fabiano.n.pereira@gmail.com>
2021-11-30 13:52:45 -0300

    Exercicio 2 somar elementos do array.

--- fundamentos/block-4-introducao-javascript-e-logica-da-programacao/dia-2-javascript-for/scripts.js
+++ fundamentos/block-4-introducao-javascript-e-logica-da-programacao/dia-2-javascript-for/scripts.js
@@ -1,5 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
 
+// Exercicio 1:
 for (let number of numbers) {
     console.log(number);
 }
+
+// Exercicio 2:
+
+for (let i = 0; i < numbers.length; i++) {
+    soma += numbers[i];
+}
+console.log('Exercicio 2, soma', soma);
